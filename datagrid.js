1fae0db434909f38bb4a02266d3b5ad5e465f136
William Clayton <wilverine7@gmail.com>
2022-10-27 08:53:09 -0600

    fully configured

--- datagrid.js
+++ datagrid.js
@@ -1,6 +1,6 @@
 // URL to your production code when you publish yoru google app script as a web app
 // You will get this URL when you publish your google apps script as a web app
-let prefix="YourWebAppURLHere" // looks like: https://script.google.com/macros/s/AKfycbz90JgtbAjrjz8c8WQGjgy8kM2GtdCAulom8XWOQyGoeX8DXM5XsmUMAcSYSSyZwwNSLq/exec
+let prefix="https://script.google.com/macros/s/AKfycbxojU28no4YDJyyEPnT-CzDLdwfyssgIYEvuB_TwlqtBLuQy5l3UJYt1PXPi2eD8kTYaw/exec" // looks like: https://script.google.com/macros/s/AKfycbz90JgtbAjrjz8c8WQGjgy8kM2GtdCAulom8XWOQyGoeX8DXM5XsmUMAcSYSSyZwwNSLq/exec
 const dealy_seconds = 4 // how long data validation messages are visible
 
 //example call: file:///C:/Users/Gove/three-tier/datagrid.html?employee
